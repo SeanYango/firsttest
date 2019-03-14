391e022105be24888409d077433e2bd895a1bbed
SeanYango <m27640133@gmail.com>
2019-03-14 13:18:57 +0800

    hw1

--- app/src/main/java/com/example/firsttest/MainActivity.java
+++ app/src/main/java/com/example/firsttest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.firsttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    int size = 30;//sp
+
+    public void bigger(View v){
+        TextView hello;
+        hello = findViewById(R.id.textView);
+        hello.setTextSize(++size);
+
+    }
+
+
 }
